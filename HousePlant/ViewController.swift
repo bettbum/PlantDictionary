3a02b4970af1bbb41ed8c6a6d43c62a26a123748
hong <bettbum@gmail.om>
2023-03-27 15:36:11 -0400

    Initial Commit

---
+++ HousePlant/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HousePlant
+//
+//  Created by Thi Hong Phuc Nguyen on 2023-03-27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
